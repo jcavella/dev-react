30010a74bd02bc02efae23e9e80562b2d3ed2921
Juan Carlos Avella <jcavella@gmail.com>
2025-07-16 13:47:06 -0500

    State activar o inactiva buton, cambiando su mensaje de activo a inactivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Counter from './componets/Counter';
+import ToggleButton from './componets/ToggleButton';
 import './App.css'
 
 function App() {
   return (
     <>
       <Counter></Counter>
+      <hr />
+      <ToggleButton></ToggleButton>
     </>
   )
 }
